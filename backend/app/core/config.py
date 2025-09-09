f301ca34456cab5ffb083a032f2eb47c9026aa7d
yahyak <yahyakaddour5@gmail.com>
2025-09-09 11:46:59 +0100

    Update config.py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     redis_host: str
     otx_api_key: str
     virustotal_api_key: str
+    LILLY_SERVER_URL : str
     class Config:
         env_file = ".env"
 
@@ -14,3 +15,4 @@ LLAMA_SERVER_URL = "http://localhost:8080/v1/chat/completions"
 LLILLY_MODEL_PATH = "/home/aziz/CTI/backend/models/lilly/lily-cybersecurity-7b-v0.2-q8_0.gguf"
 
 settings = Settings()
+
