1b12b83a8fc847ae7669a31fd6b868a72d579e80
Mohamed Aziz Aguir <mohamedaziz.aguir@outlook.com>
2025-09-24 16:12:13 +0100

    Update asrg_cron_job.py

--- backend/app/cron/asrg_cron_job.py
+++ backend/app/cron/asrg_cron_job.py
@@ -1,5 +1,5 @@
-from app.services.asrg_vuldb_service import ASRGVulnerabilityService  
 
 if __name__ == "__main__":
+    from app.services.asrg_vuldb_service import ASRGVulnerabilityService
     result = ASRGVulnerabilityService.fetch_and_index("cve")
     print(result)
